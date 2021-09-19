ca4f1730293cdb3735bdae73fb9521674776c273
Valentin Baca <valbaca@gmail.com>
2021-09-19 10:47:19 -0700

    basic command line

---
+++ src/modern_python_valbaca/console.py
@@ -0,0 +1,8 @@
+import click
+from . import __version__
+
+@click.command()
+@click.version_option(version=__version__)
+def main():
+    """The hypermodern python project"""
+    click.echo("Hello world")
