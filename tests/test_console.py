1e2cdaaf6e664978dffb26959a56776e7d2d9eda
Valentin Baca <valbaca@gmail.com>
2021-09-19 12:04:23 -0700

    unit tests with mocks

--- tests/test_console.py
+++ tests/test_console.py
@@ -8,7 +8,25 @@ from modern_python_valbaca import console
 def runner():
     return click.testing.CliRunner()
 
+@pytest.fixture
+def mock_requests_get(mocker):
+    mock = mocker.patch("requests.get")
+    mock.return_value.__enter__.return_value.json.return_value = {
+        "title": "Lorem Ipsum",
+        "extract": "Lorem ipsum dolor sit amet"
+    }
+    return mock
 
-def test_main_succeeds(runner):
+def test_main_succeeds(runner, mock_requests_get):
     result = runner.invoke(console.main)
+    # better to have a single assert per test, but this is fine for now
     assert result.exit_code == 0
+    assert "Lorem Ipsum" in result.output
+    assert mock_requests_get.called
+    args, _ = mock_requests_get.call_args
+    assert "en.wikipedia.org" in args[0]
+
+def test_main_fails_on_request_error(runner, mock_requests_get):
+    mock_requests_get.side_effect = Exception("Boom")
+    result = runner.invoke(console.main)
+    assert result.exit_code == 1
