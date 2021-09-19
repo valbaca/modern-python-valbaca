ea5d09bb9c6827b411142e75f6bdf4df5073d66e
Valentin Baca <valbaca@gmail.com>
2021-09-19 11:37:48 -0700

    Random wikipedia facts

--- src/modern_python_valbaca/console.py
+++ src/modern_python_valbaca/console.py
@@ -1,8 +1,21 @@
 import click
+import requests
+import textwrap
+
 from . import __version__
+
+API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+
 
 @click.command()
 @click.version_option(version=__version__)
 def main():
     """The hypermodern python project"""
-    click.echo("Hello world")
+    with requests.get(API_URL) as response:
+        response.raise_for_status()
+        data = response.json()
+
+    title = data["title"]
+    extract = data["extract"]
+    click.secho(title, fg="green")
+    click.echo(textwrap.fill(extract))
